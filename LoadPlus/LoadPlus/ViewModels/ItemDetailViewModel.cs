713d4b085dad8109f35723e69fbe4aa313e71b37
Fernando Inzunza <ferinzo96@gmail.com>
2020-03-02 13:32:07 -0700

    Agregar archivos de proyecto.

---
+++ LoadPlus/LoadPlus/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LoadPlus.Models;
+
+namespace LoadPlus.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
